d5bbfb8829a89def2de1c3eb773ebbc2fe4f78c2
islanderian <createdby.island@gmail.com>
2022-02-12 14:46:51 +0900

    #2.4 - Layout System

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    // <div> 대신 <View>를 사용, 모든 텍스트는 <Text> 안에 써 주어야 한다!
-    <View style={styles.container}>
-      <Text style={styles.text}>Hellooooo!!!!!</Text>
-      <StatusBar style="auto" />
+    // 모바일에서 flexDirection 의 기본값은 column
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 2, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-
-// StyleSheet.create 는 css 자동완성 기능을 제공
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 28,
-    color: "red",
-  },
-});
