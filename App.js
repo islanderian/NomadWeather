180aaa76f1f2a661d78c54f44cfc05942342027a
islanderian <createdby.island@gmail.com>
2022-02-12 13:17:15 +0900

    #2.1 - The Rules of Native

--- App.js
+++ App.js
@@ -3,18 +3,24 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
+    // <div> 대신 <View>를 사용, 모든 텍스트는 <Text> 안에 써 주어야 한다!
     <View style={styles.container}>
-      <Text>Hellooooooo!</Text>
+      <Text style={styles.text}>Hellooooo!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// StyleSheet.create 는 css 자동완성 기능을 제공
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: "red",
   },
 });
