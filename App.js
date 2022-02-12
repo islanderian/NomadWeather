b08b5555cf6cc912a414ff2b752c80859adc5bc9
islanderian <createdby.island@gmail.com>
2022-02-12 17:41:52 +0900

    #2.7 - Location

--- App.js
+++ App.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 // 모바일 스크린 너비 가져오기
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 // const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const [city, setCity] = useState("Loading...");
+
+  const ask = async () => {
+    // Permission 얻기
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setOk(false);
+    }
+    // 정확도 5로 현재 위치 (경도, 위도) 얻기
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({
+      accuracy: 5,
+    });
+    // 경도, 위도로 현재 위치의 주소(도시명 등) 얻기
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    console.log(location);
+    console.log(location[0].city);
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     // 모바일에서 flexDirection 의 기본값은 column
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       {/* ScrollView 에서는 cotentContainerStyle 을 사용해 CSS 적용해야 함 */}
       <ScrollView
